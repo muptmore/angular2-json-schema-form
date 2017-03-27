4fa8f31ea3c76da7ae70993773745da2c09214d2
Michael Uptmore <muptmore@transurban.tech>
2017-03-27 13:44:55 +1100

    update hint logic to use tooltip

--- src/frameworks/material-design/material-input.component.ts
+++ src/frameworks/material-design/material-input.component.ts
@@ -6,7 +6,7 @@ import { JsonSchemaFormService } from '../../library/json-schema-form.service';
 @Component({
   selector: 'material-input-widget',
   template: `
-    <div layout="row" [class]="options?.htmlClass">
+    <div layout="row" mdTooltip="options?.description" [mdTooltipPosition]="above" [class]="options?.htmlClass">
       <md-input-container flex>
         <input mdInput #inputControl
           [attr.aria-describedby]="'control' + layoutNode?._id + 'Status'"
@@ -29,8 +29,8 @@ import { JsonSchemaFormService } from '../../library/json-schema-form.service';
             md-prefix>{{options?.fieldAddonLeft}}</span>
           <span *ngIf="options?.fieldAddonRight"
             md-suffix>{{options?.fieldAddonRight}}</span>
-          <md-tooltip *ngIf="options?.description && !(options?.placeholder && !formControl?.dirty)">{{options?.description}}</md-tooltip>
-          <md-tooltip *ngIf="!options?.description && options?.placeholder && !formControl?.dirty">{{options?.placeholder}}</md-tooltip>
+          <md-hint *ngIf="options?.description && !(options?.placeholder && !formControl?.dirty)">{{options?.description}}</md-hint>
+          <md-hint *ngIf="!options?.description && options?.placeholder && !formControl?.dirty">{{options?.placeholder}}</md-hint>
       </md-input-container>
     </div>`,
     styles: [`mdInput { margin-top: 6px; }`],
